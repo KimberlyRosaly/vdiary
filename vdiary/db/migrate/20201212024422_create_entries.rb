d949d5075a2788840e6031100cab04968d6401b1
VQuine <01010110.v@gmail.com>
2020-12-11 23:28:52 -0500

    migration for ENTRY class - defining keys/attributes

--- vdiary/db/migrate/20201212024422_create_entries.rb
+++ vdiary/db/migrate/20201212024422_create_entries.rb
@@ -1,7 +1,9 @@
 class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
-
+      t.text :message
+      # CHARACTER CONTRAINTS : STRING - 1-255 | TEXT - 1-65536
+      t.integer :user_id
       t.timestamps null: false
     end
   end
