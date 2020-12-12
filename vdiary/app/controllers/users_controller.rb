0f648ed2e8140d75c08959d8cbe0fdea482e4b71
VQuine <01010110.v@gmail.com>
2020-12-12 00:23:58 -0500

    create SIGNUP CONTROLLER ACTION to submit new user form request, with error messages

--- vdiary/app/controllers/users_controller.rb
+++ vdiary/app/controllers/users_controller.rb
@@ -24,6 +24,33 @@ post "/login" do
 end
 #* ++++++++++++++++++++++++ LOG IN +++++++++++++++++++++++++++++
 
+
+#* ++++++++++++++++++++++++ SIGN UP +++++++++++++++++++++++++++++
+get "/signup" do
+  erb :"/users/signup.html"
+end
+
+post "/signup" do
+  # IF PARAMETERS ARE NOT BLANK & USER.NAME DOES NOT ALREADY EXIST - CREATE NEW USER
+  if !params[:user][:name].blank? && !params[:user][:password].blank?
+      user = User.find_by(:name => params[:user][:name])
+      if !user
+        new_user = User.create(params[:user])            
+        session[:user_id] = new_user.id          
+          @user = new_user
+        redirect to "/portal"
+      else
+        flash[:error] = "SIGN-UP ERROR : INVALID USERNAME"
+        redirect to "/signup"
+      end
+  else
+      flash[:error] = "BLANK FIELDS ARE INVALID"
+      redirect to "/signup"
+  end    
+end
+#* ++++++++++++++++++++++++ SIGN UP +++++++++++++++++++++++++++++
+
+
   # GET: /users
   get "/users" do
     erb :"/users/index.html"
