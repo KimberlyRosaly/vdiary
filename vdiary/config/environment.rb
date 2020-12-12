e88afcdb13fcff54885b0cc4933406d628dc076b
VQuine <01010110.v@gmail.com>
2020-12-11 22:58:14 -0500

    alter environment, use secret file if machine is in development mode

--- vdiary/config/environment.rb
+++ vdiary/config/environment.rb
@@ -1,4 +1,9 @@
+# SHORT CIRCUITING - BOOLEAN OPERATOR
 ENV['SINATRA_ENV'] ||= "development"
+
+if ENV['SINATRA_ENV'] == "development"
+  require_relative '../secrets.rb'
+end
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
