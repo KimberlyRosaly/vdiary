1404e5273ebf2dee269c539ffd47b732f05d970a
VQuine <01010110.v@gmail.com>
2020-12-14 15:03:42 -0500

    add a COLUMN to the ENTRIES TABLE for keeping track of themselves among others by the same USER

--- vdiary/db/migrate/20201212024422_create_entries.rb
+++ vdiary/db/migrate/20201212024422_create_entries.rb
@@ -3,7 +3,13 @@ class CreateEntries < ActiveRecord::Migration
     create_table :entries do |t|
       t.text :message
       # CHARACTER CONTRAINTS : STRING - 1-255 | TEXT - 1-65536
+
       t.integer :user_id
+      # FOREIGN KEY for the USER RELATIONSHIP
+
+      t.integer :number
+      # KEEP TRACK : NUMBER OF CREATED ENTRY : USER HAS A TOTAL NUMBER OF ENTRIES
+
       t.timestamps null: false
     end
   end
