26b0f8e2e53694f3eb9bdc121eb37a3243b769cc
VQuine <01010110.v@gmail.com>
2020-12-14 15:02:11 -0500

    add column to USERS TABLE that keeps track of how many entries they have ever created

--- vdiary/db/migrate/20201212042928_create_users.rb
+++ vdiary/db/migrate/20201212042928_create_users.rb
@@ -3,6 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :password_digest       
       t.string :name
+
+      t.integer :entry_counter
+      # HOW MANY ENTRIES HAVE BEEN CREATED by this USER : total thus far
+
       t.timestamps null: false
     end
   end
