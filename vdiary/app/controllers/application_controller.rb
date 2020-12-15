8f38845695951f7a648843dd9012e3cd8e9a4712
VQuine <01010110.v@gmail.com>
2020-12-14 19:23:00 -0500

    Make a spot for ERROR HANDLING to hang out in, making sure it's enabled 1st inside the CONFIGURE block

--- vdiary/app/controllers/application_controller.rb
+++ vdiary/app/controllers/application_controller.rb
@@ -13,8 +13,20 @@ class ApplicationController < Sinatra::Base
     # ▲ KEY used for ENCRYPTING COOKIEES to maintain session state- MUST be strong
     register Sinatra::Flash
     # ▲ enable FLASH MESSAGES - flash{hash}
+    set :show_exceptions, :after_handler
+    #* ▲ instead of backtrace log default - ALLOW FOR CUSTOM ERROR RESPONSE
   end
 
+  #* --------ERROR HANDLING-------------------------------HANDLE THOSE ERRORS, GURL!
+  #* ▼ DISPLAY THIS ▼ - INSTEAD of default error page with BACKTRACE LOG
+  error ActiveRecord::RecordNotFound do
+    # ACTIVERECORD ERRORS - FINDBY VS FIND
+    # IF ACTIVERECORD THROWS AN ERROR TRYING TO FIND SOMETHING (EX>@ENTRY, @USER = nil)
+    flash[:error] = "ACTIVERECORD : RECORD NOT FOUND!"
+    redirect to "/entries"
+  end
+  #* --------ERROR HANDLING-------------------------------
+  
   helpers do
     def current_user
         current_user ||= User.find_by(id: session[:user_id])
